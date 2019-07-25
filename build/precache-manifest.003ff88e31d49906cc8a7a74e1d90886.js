c570d1980c094d8feb0b56542bc44b667fe598ae
Obed Hernandez <obejuela4@gmail.com>
2019-07-25 15:14:53 -0500

    add build

---
+++ build/precache-manifest.003ff88e31d49906cc8a7a74e1d90886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4be952dee6fc360bdcae8265a8f6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b38d355029d5623709a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d95c70123a7e0b095274",
+    "url": "/static/css/main.dbf0a009.chunk.css"
+  },
+  {
+    "revision": "0b38d355029d5623709a",
+    "url": "/static/js/2.ef1d83bf.chunk.js"
+  },
+  {
+    "revision": "d95c70123a7e0b095274",
+    "url": "/static/js/main.ce0fb0b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
